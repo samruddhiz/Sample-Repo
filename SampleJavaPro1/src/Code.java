9889e495cd13b86a9b105c40d02653167419d5a4
samruddhiz <samruddhi.valavalkar@gmail.com>
2021-05-31 19:09:26 +0530

    added a new print command to main class

--- SampleJavaPro1/src/Code.java
+++ SampleJavaPro1/src/Code.java
@@ -4,6 +4,7 @@ public class Code {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Sample");
+System.out.println("Added new line of code here...");
 	}
 
 }
